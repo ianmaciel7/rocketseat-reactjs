39b3490706a984e2461065da8cd55d753f5d5cf5
Ian <52354240+ianmaciel7@users.noreply.github.com>
2020-10-06 12:45:59 -0300

    Adicionando API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import api from './services/api'
 
 import './styles.css' 
 
 import Header from './components/Header'
+import Main from './pages/main' 
 
 function App() {
   return (
    <div className="App">
       <Header/>
+      <Main/>
    </div>
   );
 }
